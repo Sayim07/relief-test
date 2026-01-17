6526dcb4418857c53fa98379606a5a51947ebc36
Sayim <sks628353@gmail.com>
2026-01-17 16:04:59 +0530

    fix

--- lib/firebase/services/index.ts
+++ lib/firebase/services/index.ts
@@ -8,4 +8,7 @@ export { userService } from './userService';
 export { donationService } from './donationService';
 export { donationVerificationService, type DonationVerificationLog } from './donationVerificationService';
 export { reliefPartnerAssignmentService } from './reliefPartnerAssignmentService';
+export { beneficiaryFundService } from './beneficiaryFundService';
+export { categoryService } from './categoryService';
+export { receiptService } from './receiptService';
 export type { BeneficiaryData, TransactionData } from '../services';
